352be3f84c77c56b5aaecaa5b2f5bfb830055e2e
Joel <jmarinos@cesde.net>
2025-08-31 21:52:11 -0500

    Inicialización

---
+++ Backend/sysmarket/sysmarket/src/main/java/com/integrador/sysmarket/SysmarketApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.sysmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysmarketApplication.class, args);
+	}
+
+}
