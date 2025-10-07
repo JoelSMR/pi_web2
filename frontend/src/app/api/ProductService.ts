f04b7df094bae98aeca369ddeae5c2162f2115fb
a <a@a.com>
2025-10-06 22:32:42 -0500

    add(AxiosInstances): ->Global axios instance factory added ->Dedicated axiosServiceClass get add (ProductCrud)/Still out the Post-Update Feats

---
+++ frontend/src/app/api/ProductService.ts
@@ -0,0 +1,32 @@
+import { axiosProductInstance } from "./AxiosService";
+
+export default class ProductService{
+    private static api=axiosProductInstance
+
+    static async getAllProducts(){
+        try{
+            const response = await this.api.get("/");
+            return response.data
+        }catch(e:unknown){
+            throw new Error(e.response?.data?.mensaje || "Error en getAll")
+        }
+    }
+
+    static async getProductById(id:number){
+        try{
+        const response= await this.api.get("/"+id.toString())
+        return response.data
+        }
+        catch(e:unknown){throw new Error(e.response?.data?.mensaje || "Error en getById")            
+        }
+    }
+
+    static async deleteProductById(id:number){
+        try{
+            const response = await this.api.delete("/"+id.toString())
+            return response.data
+        }catch(e:unknown){
+            throw new Error(e.response?.data?.mensaje? || "Error deleteById")
+        }
+    }
+}
