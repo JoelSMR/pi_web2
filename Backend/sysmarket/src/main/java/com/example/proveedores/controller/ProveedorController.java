4a889fdfb70d00d466d965b2760f58e10d02c303
Bangel0 <bangel@cesde.net>
2025-10-08 00:23:00 -0500

    feat(Finished): The rest of the problems in the files are adjusted, thus completing the creation of the supplier CRUD base

--- Backend/sysmarket/src/main/java/com/example/proveedores/controller/ProveedorController.java
+++ Backend/sysmarket/src/main/java/com/example/proveedores/controller/ProveedorController.java
@@ -1,25 +1,25 @@
 package com.example.proveedores.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import uk.co.xprl.efactura.Proveedor;
+import com.example.proveedores.entity.Proveedor;
+import com.example.proveedores.service.ProveedorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import com.example.springframework.proveedorService.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/v1/proveedor")
-public class ProveedorController<proveedorService> {
+public class ProveedorController {
 
     @Autowired
-    private proveedorService proveedorService;
+    private ProveedorService proveedorService;
 
     @GetMapping
     public List<Proveedor> getAll() {
@@ -27,7 +27,7 @@ public class ProveedorController<proveedorService> {
     }
 
     @GetMapping("/{proveedorId}")
-    public Optional <Proveedor> getBId (@PathVariable ("proveedorId") Long proveedorId) {
+    public Optional<Proveedor> getBId(@PathVariable("proveedorId") Long proveedorId) {
         return proveedorService.getProveedor(proveedorId);
     }
 
@@ -37,7 +37,7 @@ public class ProveedorController<proveedorService> {
     }
 
     @DeleteMapping("/{proveedorId}")
-    public void saveOrUpdate(@PathVariable("proveedorId") Long proveedorId) {
+    public void deleteById(@PathVariable("proveedorId") Long proveedorId) {
         proveedorService.delete(proveedorId);
     }
 }
