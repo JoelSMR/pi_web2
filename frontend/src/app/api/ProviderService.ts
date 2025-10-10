72299e31aac43cb8cbb469720cff703c5db21887
p <p@:p.com>
2025-10-09 23:24:29 -0500

    Works On(Frontend Services): ->AuthService gets add the function to checkLogin, fetching the Backed uri ->ProviderService gets instancied in the code, funcs add -> Read-Delete

---
+++ frontend/src/app/api/ProviderService.ts
@@ -0,0 +1,21 @@
+import { axiosProviderInstance } from "./AxiosServiceFactory";
+
+export class ProductService{
+    private static api= axiosProviderInstance
+
+    static getAllProviders =async()=>{
+        const data = await this.api.get("/");
+        return data.data;
+    }
+
+    static getProviderById=async(id:string|number)=>{
+        const data = await this.api.get("/"+id.toString());
+        return data.data;
+    }
+
+    static deleteProviderById=async(id:string|number)=>{
+        const data = await this.api.delete("/"+id.toString());
+        return data.data
+
+    }
+}
