45eecc8669780215c2cecd9506f79663b7d12893
Bangel0 <bangel@cesde.net>
2025-10-22 01:13:11 -0500

    feat(add small change):change folder and add folder config

--- Backend/sysmarket/src/main/java/com/example/config/CorsConfig.java
+++ Backend/sysmarket/src/main/java/com/example/config/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.example.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,7 +14,7 @@ public class CorsConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://frontend:3000") // Añade aquí tu(s) frontend(s)
+            .allowedOrigins("http://frontend:3000","http://localhost:8501") // Añade aquí tu(s) frontend(s)
             .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
             .allowedHeaders("*")
             .exposedHeaders("Location") // si necesitas exponer algún header
