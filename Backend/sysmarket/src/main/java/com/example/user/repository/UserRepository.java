e56e66a743549ba65521718ceb623d2602bb85f2
Bangel0 <bangel@cesde.net>
2025-10-27 19:32:21 -0500

    feat(add changes): A changes into folder user, of the checklogin

--- Backend/sysmarket/src/main/java/com/example/user/repository/UserRepository.java
+++ Backend/sysmarket/src/main/java/com/example/user/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.user.entity.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    Optional<User> findByCorreo(String correo);
+    Optional<User> findByNombre(String nombre);
 }
